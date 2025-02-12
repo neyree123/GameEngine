55b6bb2812a4b6d8b14d6deacb488eba1e9e908f
Destiny Treptau <54587172+neyree123@users.noreply.github.com>
2025-02-11 22:53:43 -0600

    Added a basic logging system using spdlog

--- GameEngine/src/GameEngine/EntryPoint.h
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -8,6 +8,10 @@ int main( int argc, char** argv )
 {
 	printf("Entry Point for Game Engine \n");
 
+	GameEngine::Log::init();
+	GE_CORE_WARN("Initialized Log!");
+	GE_CLIENT_INFO("Hello");
+
 	auto app = GameEngine::CreateApplication();
 	app->Run();
 	delete app;
